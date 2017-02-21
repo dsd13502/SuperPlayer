0a67446188379b165ef7102f9581d6b03c1e6eb9
im_dsd <im_dsd@126.com>
2017-02-21 17:40:09 +0800

    在作者原有项目上进了更改
    处理了中间控制器的显示问题，添加试看接口

---
+++ superplayerlibrary/src/main/java/com/superplayer/library/mediaplayer/ITryAndSeeEndListener.java
@@ -0,0 +1,13 @@
+package com.superplayer.library.mediaplayer;
+
+/**
+ * 视频试看控制器
+ * Created by im_dsd on 2017/2/21.
+ */
+
+public interface ITryAndSeeEndListener {
+    /**
+     * 视频是看结束
+     */
+    void OnTryAndSeeEndController();
+}
